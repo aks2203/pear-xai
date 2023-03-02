d544ec1bcf64e61a27cd97268f1ef727761e9904
Avi Schwarzschild <avi1@umd.edu>
2023-03-02 10:14:56 -0500

    first commit.

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# pear-xai
+# :pear: PEAR: Post-hoc Explainer Agreement Regularization
+
+A repo for training neural networks for post hoc explainer agreement. More details about the results can be found at
+<insert_link_here>
+
+
+[//]: # (## Citing Our Work)
+[//]: # ()
+[//]: # (To cite our work, please reference the appropriate paper.)
+
+## Getting Started
+
+If you are intersted in using the :pear: PEAR package to train your own models, you can install the package through pip.
+
+```$ pip install pear-xai```
+
+You should then be able to run an example in a python interpreter:
+
+```
+$ python
+>>> import pear
+>>> pear.run_example()
+```
+
+If you are interested in cloning this repository and reproducing or building on our experiments, you can install the requirements manually as follows. (This code was developed and tested with Python 3.9.5)
+
+After downloading the repository (or cloning it), install the requirements:
+
+```$ pip install -r requirements.txt```
+
+You can then open and run the Jupyter Notebook [tutorials](notebooks) on reproducing our results.
+________________________________________________________________________________________________
+
+## Contributing
+
+We believe in open-source community driven software development. Please open issues and pull requests with any questions or improvements you have.
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+import sys
+from io import open
+
+from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(here, "pear"))
+
+# Get the long description from the README file
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+# Manually install torch since the torchsort setup script requires this
+# the torch package is also listed in install_requires as this seems to be needed too
+os.system("pip install torch==1.11.0")
+
+setup(name="pear",
+      version="v0.1",
+      description="PEAR: Post-hoc Explainer Agreement Regularization",
+      url="https://arthur.ai",
+      keywords=["pytorch", "XAI", "machine learning"],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      py_modules=["pear"],
+      python_requires=">=3.9",
+      install_requires=[
+          "captum @ git+https://github.com/aks2203/captum",
+          "jupyter==1.0.0",
+          "lime==0.2.0.1",
+          "matplotlib==3.5.2",
+          "numpy==1.23.2",
+          "scikit-learn==1.1.2",
+          "seaborn==0.12.2",
+          "shap==0.41.0",
+          "sklearn==0.0",
+          "tabulate==0.8.10",
+          "torch==1.11.0",
+          "torchvision==0.12.0",
+          "tqdm==4.64.0",
+          "torchsort==0.1.9"],
+      license="MIT")
+
